4ea581994b8ca413117ae237ba09ceeb00974526
PabloTorres0 <pablo.javier.torres.martinez@gmail.com>
2024-04-18 13:46:09 -0600

    crud finalizado antes de validaciones

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,11 +1,15 @@
 import {Router} from 'express'
 import { outRequire } from '../middlewares/validateToken.js'
+import { deleteTask, updateTask, createTask, getTask, getTasks } from '../controllers/tasks.controles.js'
 
 const router = Router()
 
-router.get('/tasks',outRequire,(req,res)=>{
-    res.send('task')
-})
+router.get('/tasks',outRequire, getTasks)
+router.get('/tasks/:id',outRequire, getTask)
+router.post('/tasks',outRequire, createTask)
+router.delete('/tasks/:id',outRequire, deleteTask)
+router.put('/tasks/:id',outRequire, updateTask)
+
 
 
 export default router 
